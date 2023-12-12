9f88a1080c0b14d4302471400117ff141abe1ef3
VladRosi <a3714152241a@gmail.com>
2023-12-12 19:49:14 +0300

    add views to 'post'

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,6 +1,19 @@
 from django.db import models
+from django.db.models.query import QuerySet
 from django.utils import timezone
 from django.contrib.auth.models import User
+
+
+class PublishedManager(models.Manager):
+  def get_queryset(self):
+    return super().get_queryset()\
+                  .filter(status=Post.Status.PUBLISHED)
+
+
+class DraftManager(models.Manager):
+  def get_queryset(self):
+    return super().get_queryset()\
+                  .filter(status=Post.Status.DRAFT)
 
 
 class Post(models.Model):
@@ -11,14 +24,18 @@ class Post(models.Model):
   title = models.CharField(max_length=255)
   slug = models.SlugField(max_length=255)
   author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
-
   body = models.TextField()
   publish = models.DateTimeField(default=timezone.now)
   created = models.DateTimeField(auto_now_add=True)
   update = models.DateTimeField(auto_now=True)
   status = models.CharField(max_length=2, choices=Status.choices, default=Status.DRAFT)
 
+  objects = models.Manager()
+  published = PublishedManager()
+  draft = DraftManager()
+
   class Meta:
+    default_manager_name = 'objects'
     ordering = ['-publish']
     indexes = [
       models.Index(fields=['-publish']),
